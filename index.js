6cfa856cad51e30705950f2725e0583f1bdfc289
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-07-07 15:57:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,69 @@
-/* Your Code Here */
+"use-strict";
 
+let createEmployeeRecord = (arr) => {
+  return {
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+};
+
+let createEmployeeRecords = (array) => {
+  return array.map((e) => (createEmployeeRecord(e)))
+};
+
+let createTimeInEvent = function(dateStamp) {
+  let date = dateStamp.split(" ")[0];
+  let hour = parseInt(dateStamp.split(" ")[1], 10);
+
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour,
+    date
+  });
+
+  return this;
+};
+
+let createTimeOutEvent = function(dateStamp) {
+  let date = dateStamp.split(" ")[0];
+  let hour = parseInt(dateStamp.split(" ")[1], 10);
+
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour,
+    date
+  });
+
+  return this;
+};
+
+let hoursWorkedOnDate = function(dateStamp) {
+  let timeIn = this.timeInEvents.find((event) => (event.date === dateStamp));
+  let timeOut = this.timeOutEvents.find((event) => (event.date === dateStamp));
+
+  return (timeOut.hour - timeIn.hour)/100;
+};
+
+let wagesEarnedOnDate = function(dateStamp) {
+  let hoursWorked = hoursWorkedOnDate.call(this, dateStamp);
+  return hoursWorked * this.payPerHour;
+};
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+  return srcArray.find((employee) => (employee.firstName === firstName))
+};
+
+let calculatePayroll = function(employeeRecords) {
+  return employeeRecords.reduce((totalPayedOwed, record) => (totalPayedOwed += allWagesFor.call(record)), 0)
+};
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
  sneaky bug that we'll cover in the next few lessons!
-
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
