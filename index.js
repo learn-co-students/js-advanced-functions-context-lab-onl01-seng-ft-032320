5e1a2d23bf8a9cbab402c8428861529c7065fa1b
Christopher <reinkendigital@gmail.com>
2020-07-10 22:22:23 -0500

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,53 @@
  for you to use if you need it!
  */
 
+function createEmployeeRecord(arr) {
+    let employee = {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employee
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map(function(e) { return createEmployeeRecord(e) } )
+}
+
+let createTimeInEvent = function(dateTime) {
+    let timeIn = {
+        type: "TimeIn",
+        date: dateTime.split( ' ' )[0],
+        hour: parseInt(dateTime.split( ' ' )[1])
+    }
+    this.timeInEvents.push(timeIn)
+    return this
+}
+
+let createTimeOutEvent = function(dateTime) {
+    let timeOut = {
+        type: "TimeOut",
+        date: dateTime.split( ' ' )[0],
+        hour: parseInt(dateTime.split( ' ' )[1])
+    }
+    this.timeOutEvents.push(timeOut)
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    let eventOnDate = function(event) { return event.date === date },
+        inTime = this.timeInEvents.find(eventOnDate).hour,
+        outTime = this.timeOutEvents.find(eventOnDate).hour;
+    return (outTime - inTime) / 100
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +66,17 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(employees, name) {
+    return employees.find(emp => {
+        return emp.firstName === name
+    });
+}
+
+function calculatePayroll(employees) {
+    const reducer = (wagesTotal, emp) => {
+        return wagesTotal + allWagesFor.call(emp)
+    }
+    return employees.reduce(reducer, 0)
 }
